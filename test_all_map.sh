31060938ba23f614bd64c7b136b64c7f027e3f9c
Quentin ROSA <qrosa@e3r2p2.42.fr>
2017-05-26 02:51:49 +0200

    UPDATE -- Add colors in output test script map

--- test_all_map.sh
+++ test_all_map.sh
@@ -1,8 +1,17 @@
+RED=$(printf "\e[31m")
+GREEN=$(printf "\e[32m")
+YELLOW=$(printf "\e[33m")
+BLUE=$(printf "\e[34m")
+MAGENTA=$(printf "\e[35m")
+CYAN=$(printf "\e[36m")
+WHITE=$(printf "\e[37m")
+RESET=$(printf "\e[m")
+
 for var in `ls map_test/*`
 do
 	echo ""
-	echo "Test $var :"
+	echo "${BLUE}#${RESET} Test $var :"
 	./exec_sonda $var
 
-	[ `echo $?` == 0 ] && echo "SUCCESS" || echo "ERROR"
+	[ `echo $?` == 0 ] && echo "${CYAN}SUCCESS${RESET}" || echo "${RED}ERROR${RESET}"
 done
